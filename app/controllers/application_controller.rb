55370b127e05331feb35f2921357f37effad867f
Daniyal Ahmad <daniyal.ahmad@codingcops.com>
2024-10-10 13:23:40 +0500

    Integrated Guest Feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
+require 'ostruct'
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
+  
   include DeviseWhitelist
+  include SetSource
+  include CurrentUserConcern
+
+  
+
 end
